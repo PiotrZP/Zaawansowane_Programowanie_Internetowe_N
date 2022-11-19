49dbf587241fde8ec70b125012c5be38902e9092
Przemyslaw_Chorobski <PC@smth.smth>
2022-11-19 13:50:11 +0100

    Skończone 2 laboratorium

--- WebStore.DAL/DatabaseContext/WSDbContext.cs
+++ WebStore.DAL/DatabaseContext/WSDbContext.cs
@@ -11,8 +11,9 @@ public class WSDbContext : IdentityDbContext<User, IdentityRole<int>, int>
     public DbSet<User> User => Set<User>();
     public DbSet<BillingAddress> BillingAddress => Set<BillingAddress>();
     public DbSet<ShippingAddress> ShippingAddress => Set<ShippingAddress>();
+    public DbSet<Address> Address => Set<Address>();
     public DbSet<Category> Category => Set<Category>();
-    public DbSet<Invoice> Invoice => Set<Invoice>();
+    public virtual DbSet<Invoice> Invoice => Set<Invoice>();
     public DbSet<Order> Order => Set<Order>();
     public DbSet<OrderProduct> OrderProduct => Set<OrderProduct>();
     public DbSet<Product> Product => Set<Product>();
