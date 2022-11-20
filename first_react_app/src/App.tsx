fe557736d411c803790fbf81dbb586f91284bb91
Awww <Aw@gmail.com>
2022-11-20 12:43:00 +0100

    lab3 part2

--- first_react_app/src/App.tsx
+++ first_react_app/src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { ProductListItem } from './components/ProductListItem';
+import { ProductList } from './components/ProductList';
+import IProduct from './models/IProduct';
 
 function App() {
+  let staticProducts: Array<IProduct> = [
+    {
+      id: 1,
+      name :"DELL",
+      type : "Computers",
+      price: 4000
+    },
+    { 
+      id: 2,
+      name :"Logitech 2000",
+      type : "Mouses",
+      price: 30
+    }
+  ]
+
   return (
     <div className='App'>
-      <ProductListItem id={1} name={"PC"} type={"Computers"} price={4000}></ProductListItem>
+      <ProductList products={staticProducts}></ProductList>
     </div>
   );
 }
