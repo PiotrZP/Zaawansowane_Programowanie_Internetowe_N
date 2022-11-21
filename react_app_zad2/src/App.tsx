fc069f88af78c902f881ad59593b62e3405037e5
Olesinski <mikolajolesinski1990@gmail.com>
2022-11-21 19:08:27 +0100

    app zad 2

---
+++ react_app_zad2/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import NameOnClick from './components/NameOnClick';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <NameOnClick/>
+    </div>
+  );
+}
+
+export default App;
