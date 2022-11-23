1e9dc4a0bcd16b647d657b2cae1b8db71f879e95
Olesinski <mikolajolesinski1990@gmail.com>
2022-11-23 13:00:34 +0100

    zadanie 5

---
+++ react_app_zad5/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TwoButtons from './components/TwoButtons';
+
+function App() {
+  return (
+    <div className="App">
+      <TwoButtons/>
+    </div>
+  );
+}
+
+export default App;
