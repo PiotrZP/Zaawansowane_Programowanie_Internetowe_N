9f3e9b15ef2ba8ed27efa06865990808406f83c1
Spythere <spythere@protonmail.com>
2022-11-23 01:01:20 +0100

    Lab 3 Zad 6

--- lab3_zad2/src/components/MyComponent.tsx
+++ lab3_zad2/src/components/MyComponent.tsx
@@ -7,13 +7,14 @@ function MyComponent() {
 
   return (
     <div className="my-component">
+      <button onClick={() => setCounter(counter - 2)}>-2</button>
       <button onClick={() => setCounter(counter - 1)}>-1</button>
       <p>{counter}</p>
       <button onClick={() => setCounter(counter + 1)}>+1</button>
+      <button onClick={() => setCounter(counter + 2)}>+2</button>
     </div>
   );
 }
 
 export default MyComponent;
-
 
