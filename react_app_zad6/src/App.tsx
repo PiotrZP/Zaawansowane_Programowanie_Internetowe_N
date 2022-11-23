4c0f42d22e63a25db472a98621e9678596746763
Olesinski <mikolajolesinski1990@gmail.com>
2022-11-23 13:45:04 +0100

    zadanie 6

---
+++ react_app_zad6/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import FourButtons from './components/FourButtons';
+
+function App() {
+  return (
+    <div className="App">
+      <FourButtons/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react_app_zad6/src/components/FourButtons.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+const FourButtons = () => {
+    const [number, setNumber] = useState(0);
+
+    const handleClick = (value: number) => {
+        setNumber((prev) => (prev += value));
+    };
+
+    return (
+        <>
+        <button
+          onClick={() => {
+            handleClick(-1);
+          }}
+        >
+          -
+        </button>
+        <button
+          onClick={() => {
+            handleClick(-2);
+          }}
+        >
+          -2
+        </button>
+        <p>{number}</p>
+        <button
+          onClick={() => {
+            handleClick(1);
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            handleClick(2);
+          }}
+        >
+          +2
+        </button>
+      </>
+    );
+  };
+
+export default FourButtons;
