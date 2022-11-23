1c963d21406ce768f1b03906708b82bede065a76
Spythere <spythere@protonmail.com>
2022-11-23 01:00:42 +0100

    Lab 3 Zad 5

--- lab3_zad2/src/components/MyComponent.tsx
+++ lab3_zad2/src/components/MyComponent.tsx
@@ -2,46 +2,18 @@ import { useState } from 'react';
 
 import './MyComponent.css';
 
-const favoriteLinks = [
-  {
-    name: 'Github',
-    href: 'https://github.com/Spythere',
-  },
-  {
-    name: 'Moodle',
-    href: 'https://moodle2022.pcz.pl',
-  },
-  {
-    name: 'YouTube',
-    href: 'https://www.youtube.com/',
-  },
-  {
-    name: 'Margonem',
-    href: 'https://www.margonem.pl',
-  },
-];
-
 function MyComponent() {
-  const [chosenIndex, setChosenIndex] = useState<number>(-1);
-
-  const toggleIndex = (index: number) => {
-    if (chosenIndex == index) return setChosenIndex(-1);
-
-    return setChosenIndex(index);
-  };
+  const [counter, setCounter] = useState<number>(0);
 
   return (
-    <div>
-      <ul>
-        {favoriteLinks.map((link, i) => (
-          <li data-selected={chosenIndex == i} onClick={() => toggleIndex(i)}>
-            {link.name}: {link.href}
-          </li>
-        ))}
-      </ul>
+    <div className="my-component">
+      <button onClick={() => setCounter(counter - 1)}>-1</button>
+      <p>{counter}</p>
+      <button onClick={() => setCounter(counter + 1)}>+1</button>
     </div>
   );
 }
 
 export default MyComponent;
+
 
