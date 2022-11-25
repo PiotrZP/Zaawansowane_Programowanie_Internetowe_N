8d6caa9e0aa0d9a636e397628d78acf370716fa7
Jakub <test@example.com>
2022-11-25 23:52:04 +0100

    Finished all of react tasks

---
+++ first_react_app/src/components/zad2/Zad2.tsx
@@ -0,0 +1,17 @@
+import React, { FC, useState } from "react"
+
+export const Zad2: FC<{}> = () => {
+    const [show, setShow] = useState<boolean>(false);
+    
+    const handleShowChange = (e: React.MouseEvent<HTMLButtonElement>) => {
+        setShow(!show);
+    }
+
+    return (
+        <div>
+            <p>paragraf</p>
+            <button onClick={handleShowChange}>Pokaż imię i nazwisko</button>
+            <p style={{display: show ? 'block' : 'none' }}>Jakub Ociepa</p>
+        </div>
+    )
+}
