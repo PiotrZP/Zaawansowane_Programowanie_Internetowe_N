a7aeca3bde48f59bdccf76a5e1f1ddf3267f0bc4
Abdullah Szopen <mat.guit2017@gmail.com>
2022-12-02 21:53:00 +0100

    Update

--- first_react_app/src/components/zad2/Zad2.tsx
+++ first_react_app/src/components/zad2/Zad2.tsx
@@ -2,7 +2,7 @@ import React, { FC, useState } from "react"
 
 export const Zad2: FC<{}> = () => {
     const [show, setShow] = useState<boolean>(false);
-    
+
     const handleShowChange = (e: React.MouseEvent<HTMLButtonElement>) => {
         setShow(!show);
     }
@@ -11,7 +11,7 @@ export const Zad2: FC<{}> = () => {
         <div>
             <p>paragraf</p>
             <button onClick={handleShowChange}>Pokaż imię i nazwisko</button>
-            <p style={{display: show ? 'block' : 'none' }}>Jakub Ociepa</p>
+            <p style={{ display: show ? 'block' : 'none' }}>Krzysztof Curran</p>
         </div>
     )
 }
