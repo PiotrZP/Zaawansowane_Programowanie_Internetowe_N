0cfaa586f6402135a9974ede92fef8735383a50b
Jakub <test@example.com>
2022-12-06 00:37:10 +0100

    move route file to the separate directory, refactor code

--- WebStore.Web/ClientApp/src/index.js
+++ WebStore.Web/ClientApp/src/index.js
@@ -1,51 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
 import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App';
+import router from './router/router';
+import { RouterProvider } from 'react-router-dom';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import { AddressProvider } from './contexts/AddressContext';
-import { AddressGrid } from './components/Address/AddressGrid';
-import { AddressAdd } from './components/Address/AddressAdd';
-import { AddressEdit } from './components/Address/AddressEdit';
-import { AddressDelete } from './components/Address/AddressDelete';
-import { Root } from './components/Root';
-import { Home } from './components/Home';
-
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
-
-const router = createBrowserRouter([
-  {
-    element: <Root/>,
-    children: [
-      {
-        index: true,
-        path: '/',
-        element: <Home/>
-      },
-      {
-        path: '/address',
-        element: <AddressGrid/>
-      },
-      {
-        path: 'address/add',
-        element: <AddressAdd/>
-      },
-      {
-        path: '/address/edit/:id',
-        element: <AddressEdit/>
-      },
-      {
-        path: '/address/delete/:id',
-        element: <AddressDelete/>
-      }
-    ]
-  },
-  
-])
 
 ReactDOM.render(
   <RouterProvider router={router} />,
